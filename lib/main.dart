00401f4a2c281853ecfbb7e6a6605fbeef82be51
Peter Chacko <workpraytoday@gmail.com>
2020-09-04 21:51:49 -0500

    finished lecture 9

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower',
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('click me'),
-          backgroundColor: Colors.red[600],
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: IconButton(
+          onPressed: () {
+            print('clicked me');
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.amber,
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[400],
+      ),
+    );
+  }
+}
